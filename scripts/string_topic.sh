ecdd54c7174d0c92dd7fd71ab487560c7502f39f
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2022-01-14 01:55:26 +0900

    Implement pub1subN measurement loop

--- scripts/string_topic.sh
+++ scripts/string_topic.sh
@@ -5,10 +5,15 @@
 NUM_COMM=5
 # Time to sleep after running sub_main before running pub_main.
 SUB_PUB_INTERVAL=0.1
+
 # Maximum length of string a.k.a size of message.
 MAX_STR_LENGTH=8192
-# Current length, that will be increased by a factor of two.
-CUR_STR_LENGTH=16
+# Initial value of length, that will be increased by a factor of two.
+INI_STR_LENGTH=16
+# Maximum number of nodes
+MAX_NUM_NODES=100
+# Initial number of nodes, that will be increased by 20
+INI_NUM_NODES=20
 
 if [ $# != 1 ]; then
   echo "Usage: ./string_topic.sh <version>"
@@ -27,13 +32,44 @@ if [ $? -ne 0 ]; then
 fi
 
 # Pub1 : Sub1 test.
-NUM_SUB=1
-NUM_PUB=1
+CUR_STR_LENGTH=${INI_STR_LENGTH}
 
 while [ ${CUR_STR_LENGTH} -le ${MAX_STR_LENGTH} ]; do
+  NUM_PUB=1
+  NUM_SUB=1
+
   FILEPATH="./results/p1s1/${VERSION}/${CUR_STR_LENGTH}"
-  FILE_SUB="${FILEPATH}/sub.txt"
   FILE_PUB="${FILEPATH}/pub.txt"
+  FILE_SUB="${FILEPATH}/sub.txt"
+  mkdir -p ${FILEPATH}
+
+  CMD="mix run -e 'RclexBench.StringTopic.sub_main(\"${FILE_SUB}\", ${NUM_SUB})'"
+  eval ${CMD} &
+  PID_SUB=$!
+
+  # Wait a while.
+  sleep ${SUB_PUB_INTERVAL}
+
+  CMD="mix run -e 'RclexBench.StringTopic.pub_main(\"${FILE_PUB}\", ${NUM_PUB}, ${CUR_STR_LENGTH}, ${NUM_COMM})'"
+  eval ${CMD} &
+  PID_PUB=$!
+
+  wait $PID_SUB $PID_PUB
+
+  CUR_STR_LENGTH=$((${CUR_STR_LENGTH} * 2))
+done
+
+
+# PubN : Sub1 test.
+CUR_STR_LENGTH=${INI_STR_LENGTH}
+
+while [ ${CUR_STR_LENGTH} -le ${MAX_STR_LENGTH} ]; do
+  NUM_PUB=${INI_NUM_NODES}
+  NUM_SUB=1
+  while [ ${NUM_PUB} -le ${MAX_NUM_NODES} ]; do
+  FILEPATH="./results/pNs1/${VERSION}/${CUR_STR_LENGTH}/${NUM_PUB}"
+  FILE_PUB="${FILEPATH}/pub.txt"
+  FILE_SUB="${FILEPATH}/sub.txt"
   mkdir -p ${FILEPATH}
   
   CMD="mix run -e 'RclexBench.StringTopic.sub_main(\"${FILE_SUB}\", ${NUM_SUB})'"
@@ -49,5 +85,7 @@ while [ ${CUR_STR_LENGTH} -le ${MAX_STR_LENGTH} ]; do
 
   wait $PID_SUB $PID_PUB
 
+  NUM_PUB=$((${NUM_PUB} + 20))
+  done
   CUR_STR_LENGTH=$((${CUR_STR_LENGTH} * 2))
 done

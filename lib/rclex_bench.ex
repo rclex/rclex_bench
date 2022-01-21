075dba29148d4f3024bfd343ffe6e35800ca1140
kebus <kemu423@gmail.com>
2022-01-21 17:04:55 +0900

    add parameter for measureing cpu and memory usage

--- lib/rclex_bench.ex
+++ lib/rclex_bench.ex
@@ -7,5 +7,7 @@ defmodule RclexBench do
   defmacro eval_interval, do: 100
   defmacro eval_pub_period, do: 2_000
   defmacro eval_sub_period, do: 10_000
+  defmacro eval_pub_period_measuring_usage, do: 31_000
+  defmacro eval_sub_period_measuring_usage, do: 32_000
   defmacro logger_level, do: :warn
 end

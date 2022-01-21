3e8bcbd88828f6ae8d21c0a4ef89c423fc610e82
kebus <kemu423@gmail.com>
2022-01-21 17:05:51 +0900

    add code measuring cpu and memory usage

---
+++ lib/rclex_bench/string_topic_measuring_usage.ex
@@ -0,0 +1,130 @@
+defmodule RclexBench.StringTopicMeasuringUsage do
+  @moduledoc """
+    The benchmark for String type.
+  """
+  require RclexBench
+  @eval_interval RclexBench.eval_interval()
+  @eval_pub_period RclexBench.eval_pub_period_measuring_usage()
+  @eval_sub_period RclexBench.eval_sub_period_measuring_usage()
+  @logger_level RclexBench.logger_level()
+
+  @doc """
+    The benchmark which makes any number of publishers.
+  """
+  def pub_main(_filepath, num_node, str_length, _num_comm) do
+    Logger.configure(level: @logger_level)
+
+    # Generate file for measurement logs.
+    # File.write(filepath, "")
+    # Start ResultsServer for publisher.
+    # RclexBench.ResultsServer.start_link(:pub_server, "")
+
+    # Create nodes and register them as publishers.
+    context = Rclex.rclexinit()
+    {:ok, nodes} = Rclex.ResourceServer.create_nodes(context, 'pub_node', num_node)
+    {:ok, publishers} = Rclex.Node.create_publishers(nodes, 'testtopic', :single)
+
+    # Create and start Rclex Timer for publication.
+    {:ok, timer} =
+      Rclex.ResourceServer.create_timer(
+        &pub_callback/1,
+        {publishers, str_length},
+        @eval_interval,
+        "test_timer"
+      )
+
+    # Wait a while to finish publication.
+    Process.sleep(@eval_pub_period)
+
+    # Finalize Rclex environments.
+    Rclex.ResourceServer.stop_timer(timer)
+    Rclex.Node.finish_jobs(publishers)
+    Rclex.ResourceServer.finish_nodes(nodes)
+    Rclex.shutdown(context)
+
+    # Write results to the file.
+    # RclexBench.ResultsServer.write(:pub_server, filepath)
+    # Process.sleep(1000)
+    # RclexBench.ResultsServer.stop(:pub_server)
+  end
+
+  @doc """
+    Timer event callback function for publication.
+  """
+  def pub_callback(args) do
+    {publishers, length} = args
+    n = length(publishers)
+
+    # Prepare messages according to the number of publishers.
+    messages =
+      Enum.map(0..(n - 1), fn _ ->
+        RclexBench.Utils.random_string(length)
+      end)
+
+    # Convert messages to ROS format
+    msg_list = Rclex.initialize_msgs(n, :string)
+
+    Enum.map(0..(n - 1), fn index ->
+      Rclex.setdata(Enum.at(msg_list, index), Enum.at(messages, index), :string)
+    end)
+
+    # Publish topics after measuring system time.
+    # time = "#{System.system_time(:microsecond)}"
+    Rclex.Publisher.publish(publishers, msg_list)
+
+    ## For debugging to publishing message.
+    # IO.puts("[#{time}] published msg: #{message}")
+
+    # Store time to ResultsServer.
+    # Enum.map(0..(n - 1), fn index ->
+    #   RclexBench.ResultsServer.store(:pub_server, "#{Enum.at(messages, index)},#{time}\r\n")
+    # end)
+  end
+
+  @doc """
+    The benchmark which makes any number of subscribers.
+  """
+  def sub_main(_filepath, num_node) do
+    Logger.configure(level: @logger_level)
+
+    # Generate file for measurement logs.
+    # File.write(filepath, "")
+    # Start ResultsServer for subscriber.
+    # RclexBench.ResultsServer.start_link(:sub_server, "")
+
+    # Create nodes and register them as subscribers.
+    context = Rclex.rclexinit()
+    {:ok, nodes} = Rclex.ResourceServer.create_nodes(context, 'sub_node', num_node)
+    {:ok, subscribers} = Rclex.Node.create_subscribers(nodes, 'testtopic', :single)
+
+    # Register callback and start subscription.
+    Rclex.Subscriber.start_subscribing(subscribers, context, &sub_callback/1)
+
+    # Wait a while to finish publication.
+    Process.sleep(@eval_sub_period)
+
+    # Finalize Rclex environments.
+    Rclex.Subscriber.stop_subscribing(subscribers)
+    Rclex.Node.finish_jobs(subscribers)
+    Rclex.ResourceServer.finish_nodes(nodes)
+    Rclex.shutdown(context)
+
+    # Write results to the file.
+    # RclexBench.ResultsServer.write(:sub_server, filepath)
+    # Process.sleep(1000)
+    # RclexBench.ResultsServer.stop(:sub_server)
+  end
+
+  @doc """
+    Callback function for subscribers.
+  """
+  def sub_callback(_msg) do
+    # Measure system time just after subscribing.
+    # time = "#{System.system_time(:microsecond)}"
+
+    # recv_msg = Rclex.readdata_string(msg)
+    # RclexBench.ResultsServer.store(:sub_server, "#{recv_msg},#{time}\r\n")
+    ## For debugging to subscribing message.
+    # IO.puts("[#{time}] subscribed msg: #{recv_msg}")
+  end
+end
---
+++ scripts/string_topic_measuring_usage.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+### Configuration part.
+# The number of communication (publication) for each measurement.
+NUM_COMM=5
+# Time to sleep after running sub_main before running pub_main.
+SUB_PUB_INTERVAL=0.1
+
+# Maximum length of string a.k.a size of message.
+#MAX_STR_LENGTH=8192
+MAX_STR_LENGTH=256
+# Initial value of length, that will be increased by a factor of two.
+INI_STR_LENGTH=128
+# Maximum number of nodes
+#MAX_NUM_NODES=100
+MAX_NUM_NODES=100
+# Initial number of nodes, that will be increased by 20
+INI_NUM_NODES=20
+
+if [ $# != 1 ]; then
+  echo "Usage: ./string_topic.sh <version>"
+  exit 1
+fi
+
+VERSION=$1
+
+# Re-compile benchmark.
+rm -rf _build/ deps
+mix deps.get
+mix compile
+if [ $? -ne 0 ]; then
+  echo "Error: mix compile failed"
+  exit 1
+fi
+
+# Pub1 : Sub1 test.
+CUR_STR_LENGTH=${INI_STR_LENGTH}
+
+while [ ${CUR_STR_LENGTH} -le ${MAX_STR_LENGTH} ]; do
+  NUM_PUB=1
+  NUM_SUB=1
+
+  FILEPATH="./results/string/p1s1/${VERSION}/${CUR_STR_LENGTH}"
+  FILE_PUB="${FILEPATH}/pub.csv"
+  FILE_SUB="${FILEPATH}/sub.csv"
+  FILE_TIM="${FILEPATH}/time.csv"
+  mkdir -p ${FILEPATH}
+
+  CMD="mix run -e 'RclexBench.StringTopicMeasuringUsage.sub_main(\"${FILE_SUB}\", ${NUM_SUB})'"
+  eval ${CMD} &
+  PID_SUB=$!
+
+  # Wait a while.
+  sleep ${SUB_PUB_INTERVAL}
+
+  CMD="mix run -e 'RclexBench.StringTopicMeasuringUsage.pub_main(\"${FILE_PUB}\", ${NUM_PUB}, ${CUR_STR_LENGTH}, ${NUM_COMM})'"
+  eval ${CMD} &
+  PID_PUB=$!
+
+  sar -ur -o ${FILEPATH}/usage_${CUR_STR_LENGTH}_strings.log 1 30
+
+  wait $PID_SUB $PID_PUB
+
+  CUR_STR_LENGTH=$((${CUR_STR_LENGTH} * 2))
+done
+
+
+# PubN : Sub1 test.
+CUR_STR_LENGTH=${INI_STR_LENGTH}
+
+while [ ${CUR_STR_LENGTH} -le ${MAX_STR_LENGTH} ]; do
+  NUM_PUB=${INI_NUM_NODES}
+  NUM_SUB=1
+  while [ ${NUM_PUB} -le ${MAX_NUM_NODES} ]; do
+  FILEPATH="./results/string/pNs1/${VERSION}/${CUR_STR_LENGTH}/${NUM_PUB}"
+  FILE_PUB="${FILEPATH}/pub.csv"
+  FILE_SUB="${FILEPATH}/sub.csv"
+  FILE_TIM="${FILEPATH}/time.csv"
+  mkdir -p ${FILEPATH}
+
+  CMD="mix run -e 'RclexBench.StringTopicMeasuringUsage.sub_main(\"${FILE_SUB}\", ${NUM_SUB})'"
+  eval ${CMD} &
+  PID_SUB=$!
+
+  # Wait a while.
+  sleep ${SUB_PUB_INTERVAL}
+
+  CMD="mix run -e 'RclexBench.StringTopicMeasuringUsage.pub_main(\"${FILE_PUB}\", ${NUM_PUB}, ${CUR_STR_LENGTH}, ${NUM_COMM})'"
+  eval ${CMD} &
+  PID_PUB=$!
+
+  sar -ur -o ${FILEPATH}/usage_${CUR_STR_LENGTH}_strings.log 1 30
+
+  wait $PID_SUB $PID_PUB
+
+  NUM_PUB=$((${NUM_PUB} + 20))
+  done
+  CUR_STR_LENGTH=$((${CUR_STR_LENGTH} * 2))
+done
+
+
+# Pub1 : SubN test.
+CUR_STR_LENGTH=${INI_STR_LENGTH}
+
+while [ ${CUR_STR_LENGTH} -le ${MAX_STR_LENGTH} ]; do
+  NUM_PUB=1
+  NUM_SUB=${INI_NUM_NODES}
+  while [ ${NUM_SUB} -le ${MAX_NUM_NODES} ]; do
+  FILEPATH="./results/string/p1sN/${VERSION}/${CUR_STR_LENGTH}/${NUM_SUB}"
+  FILE_PUB="${FILEPATH}/pub.csv"
+  FILE_SUB="${FILEPATH}/sub.csv"
+  FILE_TIM="${FILEPATH}/time.csv"
+  mkdir -p ${FILEPATH}
+  
+  CMD="mix run -e 'RclexBench.StringTopicMeasuringUsage.sub_main(\"${FILE_SUB}\", ${NUM_SUB})'"
+  eval ${CMD} &
+  PID_SUB=$!
+
+  # Wait a while.
+  sleep ${SUB_PUB_INTERVAL}
+
+  CMD="mix run -e 'RclexBench.StringTopicMeasuringUsage.pub_main(\"${FILE_PUB}\", ${NUM_PUB}, ${CUR_STR_LENGTH}, ${NUM_COMM})'"
+  eval ${CMD} &
+  PID_PUB=$!
+
+  sar -ur -o ${FILEPATH}/usage_${CUR_STR_LENGTH}_strings.log 1 30
+
+  wait $PID_SUB $PID_PUB
+
+  NUM_SUB=$((${NUM_SUB} + 20))
+  done
+  CUR_STR_LENGTH=$((${CUR_STR_LENGTH} * 2))
+done

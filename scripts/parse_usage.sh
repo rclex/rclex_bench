79817d779b4de39e9562f4fb232419786517fb84
kebus <kemu423@gmail.com>
2022-01-21 18:48:57 +0900

    fix function

--- scripts/parse_usage.sh
+++ scripts/parse_usage.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-mix run -e 'RclexBench.ParseResult.parse_usage_p1s1("string",128)'
-mix run -e 'RclexBench.ParseResult.parse_usage_p1s1("string",256)'
-mix run -e 'RclexBench.ParseResult.parse_usage("string","pNs1",128)'
-mix run -e 'RclexBench.ParseResult.parse_usage("string","pNs1",256)'
-mix run -e 'RclexBench.ParseResult.parse_usage("string","p1sN",128)'
-mix run -e 'RclexBench.ParseResult.parse_usage("string","p1sN",256)'
+# mix run -e 'RclexBench.ParseResult.parse_usage_p1s1("string",128)'
+# mix run -e 'RclexBench.ParseResult.parse_usage_p1s1("string",256)'
+mix run -e 'RclexBench.ParseResult.parse_usage("string","pNs1")'
+mix run -e 'RclexBench.ParseResult.parse_usage("string","p1sN")'
